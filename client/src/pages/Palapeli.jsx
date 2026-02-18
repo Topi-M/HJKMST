561f722d49d75004e5d61e09e281c6b8d2ac7ad7
KalleHyvo <anguskaani@gmail.com>
2026-02-18 16:13:26 +0200

    rip

--- client/src/pages/Palapeli.jsx
+++ client/src/pages/Palapeli.jsx
@@ -6,7 +6,7 @@ import PalapeliFetchKuvaButton from "../components/PalapeliFetchKuvaButton.jsx";
 import PalapeliKuvaValinta from "../components/PalapeliKuvanValinta.jsx";
 import PalapeliLeaderboard from "../components/PalapeliLeaderboard.jsx";
 import PelienTimer from "../components/PelienTimer.jsx";
-import { tallennaTulos } from "../components/PalapeliTuloksenTallennus";
+import { tallennaTulos } from "../components/PalapeliTuloksenTallennus.jsx";
 
 export default function Palapeli() {
   // Default kuvan hakeminen
