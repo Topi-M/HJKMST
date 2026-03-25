3c812a949099328f0cf8e853496faed46f522f1a
KalleHyvo <anguskaani@gmail.com>
2026-03-25 14:45:08 +0200

    Uudelleenkirjoitusta

--- client/src/components/MuistipeliLogiikka.jsx
+++ client/src/components/MuistipeliLogiikka.jsx
@@ -1,11 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Card from "./Kortti";
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+import supabase from '../components/SupaBaseClient'
 
 function Game() {
   let [cardsState, setCardsState] = useState([]);
